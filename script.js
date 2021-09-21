8024b416c4bee6ff210861eec8d4beb9ac1f888b
hey-chan <liumatthew8@gmail.com>
2021-09-21 11:23:17 +1000

    change sidebar colour and placeholder text in search bar

--- script.js
+++ script.js
@@ -129,3 +129,28 @@ function changeWords(diets) {
   console.log(`after: ${badgeOutput}`);
   return badgeOutput;
 }
+
+
+// Front end sidebar
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  //toggle//
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    //animate link
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.2s ease forwards ${
+          index / 6}s`;
+      }
+    });
+  });
+};
+
+navSlide();
